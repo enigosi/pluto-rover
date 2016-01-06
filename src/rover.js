6b30506cc833de0d10779aceaf0c04772a12fcbe
Wojtek <enigosi@gmail.com>
2016-01-06 22:54:57 +0000

    implement rotation functionality

--- src/rover.js
+++ src/rover.js
@@ -6,7 +6,8 @@ var Rover = (function() {
 
 	return  {
 		land: init,
-		reportPosition: reportPosition
+		reportPosition: reportPosition,
+		getCommands: getCommands
 	}
 
 
@@ -21,6 +22,38 @@ var Rover = (function() {
 
 	function reportPosition() {
 		return position;
+	}
+
+	function getCommands(commands) {
+		Array.prototype.map.call(commands, processCommand);
+	}
+
+	function processCommand(command) {
+
+		if (command == 'F' || command == 'B') {
+			move(command);
+		} else {
+			rotate(command);
+		}
+	}
+
+	function rotate(command) {
+
+		var directions = ['N', 'E', 'S', 'W'];
+
+		var direction = directions.indexOf(position.d);
+
+		// -1/1 +4
+		direction += command === 'L' ? 3 : 5;
+
+		direction = direction % 4;
+
+		position.d = directions[direction];
+
+	}
+
+	function move(command) {
+
 	}
 
 })();
