4b8a31aac4abb7c674e3747d7c24584eb81d141d
Wojtek <enigosi@gmail.com>
2016-01-06 22:41:24 +0000

    add tests - rotation

--- spec/roverSpec.js
+++ spec/roverSpec.js
@@ -29,6 +29,44 @@ describe('Pluto Rover', function() {
 
 	})
 
+	describe('Rover can rotate', function() {
 
+		it('it should be able to rotate', function() {
+
+			Rover.getCommands('R');
+			var position = Rover.reportPosition();
+
+			expect(position.d).toBe('E');
+
+		})
+
+		it('it should be able to make full rotation', function() {
+
+			Rover.getCommands('RRRR');
+			var position = Rover.reportPosition();
+
+			expect(position.d).toBe('N');
+
+		})
+
+		it('it should be able to make full rotation in both directions', function() {
+
+			Rover.getCommands('LLLL');
+			var position = Rover.reportPosition();
+
+			expect(position.d).toBe('N');
+
+		})
+
+		it('it should be able to make crazy spins', function() {
+
+			Rover.getCommands('LLRLRRRRLRLRLLRLLRLRRRRLRLRLLR');
+			var position = Rover.reportPosition();
+
+			expect(position.d).toBe('S');
+
+		})
+
+	});
 
 })
